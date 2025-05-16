19c228bc8cbcc6a13bb8944ac259c86f1e8a55f4
Byron Bogaert <byron@sftechguy.com>
2025-05-15 19:27:02 -0700

    Using different cloud build module

--- terraform/sourcerepo/main.tf
+++ terraform/sourcerepo/main.tf
@@ -1,6 +1,7 @@
 # Module to provision Cloud Source Repository and service account for VM access
-resource "google_cloudbuildv2_repository" "app_repo" {
-  project        = var.project_id
-  location       = "global"
-  repository_id  = var.repo_name
+resource "google_cloudbuild_repository" "app_repo" {
+  project       = var.project_id
+  location      = "global"
+  repository_id = var.repo_name
+  remote_uri    = "https://source.developers.google.com/p/${var.project_id}/r/${var.repo_name}"
 }
