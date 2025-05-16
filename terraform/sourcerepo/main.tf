9b90ad878c2f9d946137234d7fe69df8fcd42c1a
Byron Bogaert <byron@sftechguy.com>
2025-05-15 19:40:32 -0700

    Updated parent_connection

--- terraform/sourcerepo/main.tf
+++ terraform/sourcerepo/main.tf
@@ -2,6 +2,6 @@
 resource "google_cloudbuildv2_repository" "app_repo" {
   provider          = google-beta
   name              = var.repo_name
-  parent_connection = "projects/${var.project_id}/locations/global"
+  parent_connection = "projects/${var.project_id}/locations/global/connections/_internal"
   remote_uri        = "https://source.developers.google.com/p/${var.project_id}/r/${var.repo_name}"
 }
