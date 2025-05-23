ea1f7bb617538295c33913c6723af791d511633c
Byron Bogaert <byron@sftechguy.com>
2025-05-23 13:04:57 -0700

    Flipping this WIP to public

--- terraform/infra/variables.tf
+++ terraform/infra/variables.tf
@@ -32,3 +32,34 @@ variable "app_port" {
   description = "Port for HTTP ingress to the application"
   default     = 80
 }
+
+// GCP secrets variables
+variable "enphase_local_token" {
+  type        = string
+  description = "Enphase local token"
+  sensitive   = true
+}
+
+variable "envoy_host" {
+  type        = string
+  description = "Envoy host"
+  sensitive   = true
+}
+
+variable "ts_authkey" {
+  type        = string
+  description = "TS auth key"
+  sensitive   = true
+}
+
+variable "influxdb_admin_password" {
+  type        = string
+  description = "InfluxDB admin password"
+  sensitive   = true
+}
+
+variable "influxdb_admin_token" {
+  type        = string
+  description = "InfluxDB admin token"
+  sensitive   = true
+}
